ecd3828145e1c125324b3498966aeff7578529d6
klx <klxcoder@gmail.com>
2021-09-05 20:35:14 +0700

    for-u

---
+++ index.js
@@ -0,0 +1,19 @@
+const img = document.querySelector('img');
+const audio = document.getElementById("audio");
+let audioPlaying = false;
+let interval = null;
+img.onclick = () => {
+    if(audioPlaying) {
+        audio.pause();
+        audioPlaying = false;
+        clearInterval(interval);
+    } else {
+        audio.play();
+        let angle = getComputedStyle(document.documentElement).getPropertyValue('--angle');
+        interval = setInterval(() => {
+            angle++;
+            document.documentElement.style.setProperty('--angle', angle);
+        }, 10);
+        audioPlaying = true;
+    }
+}
